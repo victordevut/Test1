dc6a1c932af7724e317a0cd29fb87e0ff1f27261
Daniel Villalba Mendoza <danielmendoza_villalba@outlook.com>
2017-01-17 14:19:08 -0600

    Segundo hola

--- Test1/Form1.Designer.cs
+++ Test1/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.SuspendLayout();
             // 
             // labelControl1
@@ -39,11 +40,20 @@
             this.labelControl1.TabIndex = 0;
             this.labelControl1.Text = "Hola";
             // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(12, 43);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(68, 13);
+            this.labelControl2.TabIndex = 1;
+            this.labelControl2.Text = "Hola de Daniel";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(632, 278);
+            this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.labelControl1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +65,7 @@
         #endregion
 
         private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
     }
 }
 
